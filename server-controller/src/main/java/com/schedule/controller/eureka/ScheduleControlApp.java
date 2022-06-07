7060bac5760799bad35acb4284a1dc9c7e33f674
MarlboroChu <marlboro.chu@gmail.com>
2022-06-07 14:49:05 +0800

    start

---
+++ server-controller/src/main/java/com/schedule/controller/eureka/ScheduleControlApp.java
@@ -0,0 +1,21 @@
+package com.schedule.controller.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ *
+ * @author marlboro.chu@gmail.com
+ */
+@SpringBootApplication(scanBasePackages = { "com.schedule.controller.eureka" })
+@EnableEurekaServer
+public class ScheduleControlApp {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(ScheduleControlApp.class, args);
+	}
+	
+}
+
+
